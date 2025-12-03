ea1c3f621988a57fca9b31d0dfd4d65a03c6cfb7
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2025-12-03 17:19:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+import { createStore } from "redux";
+const initialState={
+    value:0
+}
+function appReducer(prevState =initialState,action){
+    switch(action.type){
+        case 'increment':
+      return { value: prevState.value + 1 };
+    case 'decrement':
+      return { value: prevState.value - 1 };
+    default:
+      return prevState;
+    }
+}
+const store =createStore( appReducer)
+
+const valueLabel = document.getElementById('value');
+const btnInc = document.getElementById('increment');
+const btnDec = document.getElementById('decrement');
+
+store.subscribe(()=>{
+    valueLabel.textContent=store.getState().value
+})
+
+btnInc.onclick=()=>store.dispatch({type:'increment'})
+btnDec.onclick=()=>store.dispatch({type:'decrement'})
